b1813b5c2514c095c7fc79b94d584a5f2a00d9c6
Janne <janne.siltanen@tuni.fi>
2020-11-19 19:06:15 +0200

    Korjattu

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,10 +1,5 @@
-
-def debug_print(debug_msg=None, **kwargs):
-
-    if debug_msg:
-        print(debug_msg)
-
-    for key, value in kwargs.items():
+ 
+@@ -8,6 +8,40 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
@@ -45,15 +40,7 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-    input_list = input_str.split(",")
-    debug_print(input_list=input_list)
-
-    value_list = []
-    for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError as err:
-            print("Invalid input.")
+@@ -23,3 +57,6 @@ if __name__ == "__main__":
             quit(1)
 
     debug_print(value_list=value_list)
